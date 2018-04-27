171b71a7cc571c0d600cb87035e1d3582fe6f5e3
Rebecca Steorts <beka@stat.duke.edu>
2018-04-27 15:50:35 -0400

    fixed bug.

--- 00-setup/packages_install.R
+++ 00-setup/packages_install.R
@@ -3,5 +3,5 @@ install.packages(c("devtools", "RecordLinkage", "blink", "knitr", "ggplot2",
                    "igraph", "textreuse", "tokenizers", "numbers"))
 
 ## install data package
-devtools::install_git("https://github.com/ses109/RLdata")
+devtools::install_git("https://github.com/resteorts/RLdata")
 
