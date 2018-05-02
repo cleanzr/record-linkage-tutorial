555557af71566d16aba5ddcb844cffc2bb7516f7
Andee Kaplan <andee.kaplan@gmail.com>
2018-05-01 21:51:56 -0400

    fix dates

--- docs/code/packages_install.R
+++ docs/code/packages_install.R
@@ -1,6 +1,7 @@
 ## install packages
-install.packages(c("devtools", "RecordLinkage", "blink", "knitr", "ggplot2", "igraph", "textreuse", "tokenizers", "numbers"))
+install.packages(c("devtools", "RecordLinkage", "blink", "knitr", "ggplot2",
+                   "igraph", "textreuse", "tokenizers", "numbers"))
 
 ## install data package
-devtools::install_github("ses109/RLdata")
+devtools::install_git("https://github.com/resteorts/RLdata")
 
